66a9284a6fa13fed1f1310bfa4e52fe8708aeef0
jpg-consulting <opensource@jpg-consulting.es>
2016-10-12 20:58:59 +0200

    Fix ethernal loading on error.

--- modules/custom_fields/manage.js.php
+++ modules/custom_fields/manage.js.php
@@ -23,6 +23,9 @@ var edit_tooltip = "{/literal}{$LANG.edit_view_tooltip} {$invoices.preference.pr
 			{
 			url: 'index.php?module=custom_fields&view=xml',
 			dataType: 'xml',
+			'onError': function(data) {
+				$("#manageGrid").flexAddData(null);
+			},
 			colModel : [
 				{display: '{/literal}{$LANG.actions}{literal}', name : 'actions', width : 10 * percentage_width, sortable : false, align: 'center'},
 				{display: '{/literal}{$LANG.id}{literal}', name : 'cf_id', width : 10 * percentage_width, sortable : false, align: 'left'},
