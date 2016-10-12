db2edf8b11b9ff371f26e85b8533d6bba1fa9441
jpg-consulting <opensource@jpg-consulting.es>
2016-10-12 22:53:39 +0200

    code cleanup

--- modules/customFields/manage.php
+++ modules/customFields/manage.php
@@ -1,19 +1,19 @@
 <?php
 /*
-* Script: manage.php
-* 	new manage custom fields page
-*
-* Authors:
-*	 Nicolas Ruflin
-*
-* Last edited:
-* 	 2007-07-19
-*
-* License:
-*	 GPL v2 or above
-*
-* Website:
-* 	http://www.simpleinvoices.org
+ * Script: manage.php
+ * 	new manage custom fields page
+ *
+ * Authors:
+ *	 Nicolas Ruflin
+ *
+ * Last edited:
+ * 	 2007-07-19
+ *
+ * License:
+ *	 GPL v2 or above
+ *
+ * Website:
+ * 	http://www.simpleinvoices.org
  */
 
 if(isset($_POST['save'])) {
@@ -23,4 +23,3 @@ if(isset($_POST['save'])) {
 ini_set("display_errors","On");
 //Note: If input is language specific it has to be in the form: {$LANG['value']} or {$LANG["value"]}
 
-?>
--- modules/customFields/plugins/InvoiceNumber.php
+++ modules/customFields/plugins/InvoiceNumber.php
@@ -16,53 +16,52 @@
  * 	 http://www.simpleinvoices.org
  */
 
-class InvoiceNumber extends CustomField {
-	
-	function InvoiceNumber() {
-		parent::CustomField(4,"InvoiceNumber");
-	}
-	
-	function printOutput($id) {
-		$values = getCustomFieldValues($id);
-		echo $name.": ".$values['description'];
-	}
-	
-	function printInputField($id,$itemId) {		
-		$description = $this->getDescription($id);
-		$name = $this->getFormName($id);
-		
-		if($itemId != "") {
-			$value = $this->getFieldValue($id,$itemId);
-		}
-		else {
-			$last = $this->getLastValue();
-			$year = date("Y");
+class InvoiceNumber extends CustomField 
+{	
+    function InvoiceNumber() 
+    {
+        parent::CustomField(4,"InvoiceNumber");
+    }
+    
+    function printOutput($id) 
+    {
+        $values = getCustomFieldValues($id);
+        echo $name.": ".$values['description'];
+    }
+    
+    function printInputField($id,$itemId) 
+    {		
+        $description = $this->getDescription($id);
+        $name = $this->getFormName($id);
+        
+        if($itemId != "") {
+            $value = $this->getFieldValue($id,$itemId);
+        } else {
+            $last = $this->getLastValue();
+            $year = date("Y");
 			
-			if(preg_match("/([0-9]+)-([0-9]{4})/",$last,$match)) {
-				if($year == $match[2]) {
-					$number = $match[1]+1;
-					$value = $number."-".$year;
-				}
-				else {
-					$value = "1-".$year;
-				}
-			}
-			else {
-				$value = "1-".$year;
-			}
-				
+            if(preg_match("/([0-9]+)-([0-9]{4})/",$last,$match)) {
+                if($year == $match[2]) {
+                    $number = $match[1]+1;
+                    $value = $number."-".$year;
+                } else {
+                    $value = "1-".$year;
+                }
+            } else {
+                $value = "1-".$year;
+            }
 		}
-		
-		echo "<tr><td>".htmlsafe($description)."</td><td><input name='".htmlsafe($name)."' value='".htmlsafe($value)."' type='hidden'>".htmlsafe($value)."</td></tr>";
-	}
-	
-	function getLastValue() {
-		$sql = "SELECT value FROM ".TB_PREFIX."customFieldValues WHERE customFieldId = 7 ORDER BY id DESC LIMIT 1;";
-		$sth = dbQuery($sql);
-		$result = $sth->fetch();
-		error_log($sql);
-		return $result['value'];
-	}
+        
+        echo "<tr><td>".htmlsafe($description)."</td><td><input name='".htmlsafe($name)."' value='".htmlsafe($value)."' type='hidden'>".htmlsafe($value)."</td></tr>";
+    }
+    
+    function getLastValue() 
+    {
+        $sql = "SELECT value FROM ".TB_PREFIX."customFieldValues WHERE customFieldId = 7 ORDER BY id DESC LIMIT 1;";
+        $sth = dbQuery($sql);
+        $result = $sth->fetch();
+        error_log($sql);
+        return $result['value'];
+    }
 }
 
-?>
