2e0f7a3053b7acf0c011552c470d1bdd06d8f4da
jpg-consulting <opensource@jpg-consulting.es>
2016-10-18 02:01:01 +0200

    code cleanup

--- include/init.php
+++ include/init.php
@@ -188,47 +188,8 @@ checkConnection();
 include_once('./include/manageCustomFields.php');
 include_once("./include/validation.php");
 
-//if authentication enabled then do acl check etc..
-if ($config->authentication->enabled == 1 )
-{
-    $acl = $serviceManager->get('SimpleInvoices\Permission\Acl');
-	
-    $acl_view   = (isset($_GET['view']) ? $_GET['view'] : null);
-    $acl_action = (isset($_GET['action']) ? $_GET['action'] : null);
-    
-    if (empty($acl_action)) {
-        // no action is given
-        if (!empty($acl_view)) {
-            // view is available with no action
-            $checkPermission = $acl->isAllowed($auth_session->role_name, $module, $acl_view) ?  "allowed" : "denied"; // allowed
-        }
-    } else {
-        // action available
-        $checkPermission = $acl->isAllowed($auth_session->role_name, $module, $acl_action) ?  "allowed" : "denied"; // allowed
-    }
-    
-    //basic customer page check
-    if( ($auth_session->role_name =='customer') AND ($module == 'customers') AND ($_GET['id'] != $auth_session->user_id) )
-    {
-        $checkPermission = "denied";
-    }
-    
-    //customer invoice page add/edit check since no acl for invoices
-    if( ($auth_session->role_name =='customer') && ($module == 'invoices') ) {
-        if (   $acl_view == 'itemised' || $acl_view == 'total' || $acl_view == 'consulting' || $acl_action == 'view' || ($acl_action != '' && isset($_GET['id']) && $_GET['id'] != $auth_session->user_id) ) {
-            $checkPermission = "denied";
-        }
-    }
-    
-    $checkPermission == "denied" ? exit($LANG['denied_page']) :"" ;
-}
-
 //get the url - used for templates / pdf
 $siUrl = getURL();
-//zend db
-
-// Get extensions from DB, and update config array
-
 
 //If using the folowing line, the DB settings should be appended to the config array, instead of replacing it (NOT TESTED!)
 //$config->extension() = $DB_extensions;
