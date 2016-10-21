67c65b2fb14ebf72e6db5fec529220baaac86ede
jpg-consulting <opensource@jpg-consulting.es>
2016-10-22 01:29:14 +0200

    using intl extension

--- modules/system_defaults/manage.php
+++ modules/system_defaults/manage.php
@@ -5,28 +5,21 @@ checkLogin();
 
 //gets the long language name out of the short name
 $lang = getDefaultLanguage();
-$languages = getLanguageList();
-foreach($languages as $language) {
-	if($language->shortname == $lang) {
-		$lang = $language->name;
-		break;
-	}
+if (!empty($lang)) {
+    $lang = \Locale::getDisplayName($lang, $lang);
 }
 
-
-$smarty -> assign("defaults", getSystemDefaults());
-$smarty -> assign("defaultBiller", getDefaultBiller());
-$smarty -> assign("defaultCustomer", getDefaultCustomer());
-$smarty -> assign("defaultTax", getDefaultTax());
-$smarty -> assign("defaultPreference", getDefaultPreference());
-$smarty -> assign("defaultPaymentType", getDefaultPaymentType());
-$smarty -> assign("defaultDelete", getDefaultDelete());
-$smarty -> assign("defaultLogging", getDefaultLogging());
-$smarty -> assign("defaultInventory", getDefaultInventory());
-$smarty -> assign("defaultProductAttributes", getDefaultProductAttributes());
-$smarty -> assign("defaultLargeDataset", getDefaultLargeDataset());
-$smarty -> assign("defaultLanguage", $lang);
-
-$smarty -> assign('pageActive', 'system_default');
-$smarty -> assign('active_tab', '#setting');
-?>
+$smarty->assign("defaults", getSystemDefaults());
+$smarty->assign("defaultBiller", getDefaultBiller());
+$smarty->assign("defaultCustomer", getDefaultCustomer());
+$smarty->assign("defaultTax", getDefaultTax());
+$smarty->assign("defaultPreference", getDefaultPreference());
+$smarty->assign("defaultPaymentType", getDefaultPaymentType());
+$smarty->assign("defaultDelete", getDefaultDelete());
+$smarty->assign("defaultLogging", getDefaultLogging());
+$smarty->assign("defaultInventory", getDefaultInventory());
+$smarty->assign("defaultProductAttributes", getDefaultProductAttributes());
+$smarty->assign("defaultLargeDataset", getDefaultLargeDataset());
+$smarty->assign("defaultLanguage", $lang);
+$smarty->assign('pageActive', 'system_default');
+$smarty->assign('active_tab', '#setting');
