1a7efdcaeccfb76ff84b0aa0fe19638c60ed3932
jpg-consulting <unaputacuenta@hotmail.com>
2016-10-31 17:25:05 +0100

    Adding Zf3 code

---
+++ include/SimpleInvoices/ModuleManager/Exception/MissingDependencyModuleException.php
@@ -0,0 +1,9 @@
+<?php
+namespace SimpleInvoices\ModuleManager\Exception;
+
+/**
+ * Runtime Exception
+ */
+class MissingDependencyModuleException extends RuntimeException implements ExceptionInterface
+{
+}
